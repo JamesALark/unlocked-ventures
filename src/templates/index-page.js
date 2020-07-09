38a84f3cbd24e6f42a4113fa502b62af46effd08
James-Lark <james.lark@pwc.com>
2020-07-09 16:34:46 +1000

    changed header to solid colour

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -18,11 +18,8 @@ export const IndexPageTemplate = ({
 }) => (
   <div>
     <div
-      className="full-width-image margin-top-0"
+      className="full-width-image margin-top-0 has-background-black"
       style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
         backgroundPosition: `top left`,
         backgroundAttachment: `fixed`
       }}
